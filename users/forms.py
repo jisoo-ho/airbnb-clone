50b16b8ceaf9ce960cf7cd7c9910485bdf5afbe6
jisoo-ho <hojysoo@naver.com>
2020-05-14 20:11:17 +0900

    setting login csrf_token & function

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -6,3 +7,16 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     # 위젯을 사용하여 패스워드를 점 형태로 바꿔준다.
     password = forms.CharField(widget=forms.PasswordInput)
+
+    # 내부 함수를 사용한 것(clean_email, password는 임시로 지정한 명칭이 아니다.)
+    # 전달받은 이메일과 비밀번호가 에러가 없을 경우 views.py에서 is_valid()가 True로 작동
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("유저가 존재하지 않습니다.(User doesn't exist)")
+
+    def clean_password(self):
+        return "lalalalalalal"
--- users/views.py
+++ users/views.py
@@ -7,9 +7,11 @@ class LoginView(View):
     """클래스 기반 뷰"""
 
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "itn@las.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)  # 모든 필드를 정리해준 결과
+        return render(request, "users/login.html", {"form": form})
