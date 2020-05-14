a6fdf3ba9053ec00c9417430e5df6ff4e6df7d64
jisoo-ho <hojysoo@naver.com>
2020-05-14 20:35:50 +0900

    login logout with csrf token function

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = "users"
 # Djaneiro - Django Snippets 익스텐션 설치
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
@@ -13,6 +14,19 @@ class LoginView(View):
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)  # 모든 필드를 정리해준 결과
-            # {'email': 'hojysoo@naver.com', 'password': '1234'} 로 확인할 수 있다.
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            # authenticate : django가 쿠키 관리와 DB 연결을 자동으로 해준다.
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))  # 로그인 완료시 홈 화면으로 이동
+                # 관리자가 로그인 한 경우 admin 페이지까지 함께 로그인 완료 처리
+
         return render(request, "users/login.html", {"form": form})
+
+
+# 로그아웃 메서드(로그아웃 시 홈으로 이동)
+def log_out(request):
+    logout(request)  # 이 줄의 로그아웃은 임포트한 함수이므로 주의
+    return redirect(reverse("core:home"))
