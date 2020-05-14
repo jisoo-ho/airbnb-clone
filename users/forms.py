d1d1d17f1e9dc2eb0940ea783af47c8cbb239b2c
jisoo-ho <hojysoo@naver.com>
2020-05-14 21:08:53 +0900

    sign up function making & saving account

--- users/forms.py
+++ users/forms.py
@@ -26,3 +26,42 @@ class LoginForm(forms.Form):
             self.add_error(
                 "email", forms.ValidationError("유저가 존재하지 않습니다.(User doesn't exist)")
             )
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "이미 존재하는 이메일 입니다.(User already exists with that email)"
+            )
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):  # 양식의 필드를 순차적으로 clean하기 때문에 명칭을 다르게 한다.
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError(
+                "비밀번호가 일치하지 않습니다.(Password confirmation does not match)"
+            )
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user = models.User.objects.create_user(email, email, password)  # 암호화 하여 저장
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
-from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
@@ -27,3 +26,20 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)  # 이 줄의 로그아웃은 임포트한 함수이므로 주의
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")  # 회원가입 성공 시 이동 화면(홈)
+    initial = {"first_name": "jisoo", "last_name": "ho", "email": "hojysoo@naver.com"}
+
+    def form_valid(self, form):
+        form.save()  # 저장 함수 실행
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
