79480a23f786787568a8d2214bf54f545098b0a5
jisoo-ho <hojysoo@naver.com>
2020-05-20 22:49:20 +0900

    making room list(main)

--- rooms/views.py
+++ rooms/views.py
@@ -9,7 +9,7 @@ class HomeView(ListView):
     """ 홈 뷰 정의 """
 
     model = models.Room  # 전체 방 목록 호출
-    paginate_by = 10  # 한 번에 보여줄 방 개수
+    paginate_by = 12  # 한 번에 보여줄 방 개수
     paginate_orphans = 5  # 5개 미만의 방이 페이지에 있다면 이전 페이지에서 출력
     ordering = "created"  # 생성순 정렬
     context_object_name = "rooms"
